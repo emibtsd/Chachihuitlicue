67e75cb13c5a95f56a8fff1e05bd0492c4c7731d
emibtsd <emiliano.bustos@udem.edu>
2026-06-25 03:09:18 -0600

    Add files via upload

---
+++ Práctica 3/Actividad/main.c
@@ -0,0 +1,53 @@
+  #include <xc.h>         // Biblioteca principal del compilador XC8
+
+//=============================================================================
+// CONFIGURACIÓN DE BITS DE CONFIGURACIÓN (FUSES)
+//=============================================================================
+
+// Selección de oscilador (usar XT si estas usando un cristal de 4 MHz)
+#pragma config FOSC = HS        // Oscillator Selection bits (XT oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (enabled)
+#pragma config LVP = OFF        // Low-Voltage Programming Enable bit (disabled)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection (disabled)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable (disabled)
+#pragma config CP = OFF         // Flash Program Memory Code Protection (disabled)
+
+//=============================================================================
+// DEFINICIONES
+//=============================================================================
+
+#define _XTAL_FREQ 8000000      // Frecuencia del oscilador (para __delay_ms y __delay_us)
+
+unsigned char numb[] = {
+    0x3F, // 0
+    0x06, // 1
+    0x5B, // 2
+    0x4F, // 3
+    0x66, // 4
+    0x6D, // 5
+    0x7D, // 6
+    0x07, // 7
+    0x7F, // 8
+    0x6F, // 9
+    0x77, // A
+    0x7C, // b  
+    0x39, // C  
+    0x5E, // d
+    0x79, // E  
+    0x71  // F  
+};
+
+void main(void){
+    TRISD= 0b00000000;
+    PORTD= 0;
+    
+    while (1){
+        for(unsigned char i = 0; i < 16; i++){ 
+                            
+            PORTD = numb[i];  
+            __delay_ms(250);
+        }
+    }
+}
